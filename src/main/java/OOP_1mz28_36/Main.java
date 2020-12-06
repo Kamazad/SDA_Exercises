f6865e10dec7e8940b8c03db007e9befa2ccc74c
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2020-12-06 12:15:36 +0100

    Adding instance of SportCar to Main.java

--- src/main/java/OOP_1mz28_36/Main.java
+++ src/main/java/OOP_1mz28_36/Main.java
@@ -1,4 +1,8 @@
 package OOP_1mz28_36;
 
 public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine(2.2D, 532D, 10.0D);
+        SportCar sportCar1 = new SportCar("Opel", "Corsa", "color", 2, engine);
+    }
 }
