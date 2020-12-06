8fe68cfa3a777989a6c8742cb8610a16bd6c05c1
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2020-12-06 10:47:45 +0100

    Editing SetTraining

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 import collection_training.ListTraining;
+import collection_training.SetTraining;
 
 public class Main {
     public static void main(String[] args) {
         ListTraining.trainList();
-
+        SetTraining.trainSet();
     }
 }
