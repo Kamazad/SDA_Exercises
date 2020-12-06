c4df6bbcc8fbbe9c835619418d7b35eed5fcf4e8
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2020-12-06 11:52:02 +0100

    Created engine

---
+++ src/main/java/OOP_1mz28_36/Engine.java
@@ -0,0 +1,31 @@
+package OOP_1mz28_36;
+//Ex 28
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Engine {
+    private Double capacity;
+    @Getter
+    @Setter
+    private Double horsePower;
+    @Getter
+    @Setter
+    private Double fuelConsumption;
+
+    //Ex 29
+    public Engine(Double capacity, Double horsePower, Double fuelConsumption) {
+        this.capacity = capacity;
+        this.horsePower = horsePower;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    //Ex 30
+    public void setCapacity(Double capacity) {
+        this.capacity = capacity;
+    }
+
+    public Double getCapacity() {
+        return this.capacity;
+    }
+}
